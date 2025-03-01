a8d703ee659a320328ecdcf9bdbe95c76f131f54
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2025-03-01 15:12:34 +0200

    Created BlazorHelloWorldApp

---
+++ BlazorHelloWorldApp/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorHelloWorldApp
+{
+    public class Person
+    {
+        /* Пример 13. Привязка моделей 
+        public string Name { get; set; } = "";
+        public int Age { get; set; }*/
+
+        /* Пример 14. Привязка моделей 2 */
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public DateTime Day { get; set; } = DateTime.Now;
+        public bool IsWorking { get; set; }
+        public string Subject { get; set; } = "";
+        public string[] Courses { get; set; } = [];
+    }
+}
