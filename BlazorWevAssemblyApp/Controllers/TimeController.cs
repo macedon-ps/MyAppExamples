90089a66830575ad25111e50a35afa2c3c099130
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2025-03-03 14:11:28 +0200

    Created BlazorWebApiApp

---
+++ BlazorWevAssemblyApp/Controllers/TimeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+
+namespace BlazorWebAssemblyApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetTime()
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+    }
+   
+}
--- BlazorWevAssemblyApp/Program.cs
+++ BlazorWevAssemblyApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWebAssemblyApp;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
