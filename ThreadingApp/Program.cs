9571a9e68469807582f94a86c15b6f66db09efaa
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2025-05-03 14:23:17 +0300

    changed ThreadingApp

--- ThreadingApp/Program.cs
+++ ThreadingApp/Program.cs
@@ -169,6 +169,30 @@ void Print()
 }
 */
 
+/* Пример 8.1. Несколько потоков 2, с блокированием одного потока методом lock(Lock _lockObj)
+int x = 0;  // некоторый общий ресурс
+Lock _lockObj = new(); // объект-заглушка для синхронизации доступа
+// запускаем пять потоков
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new(Print);
+    myThread.Name = $"Поток {i}";
+    myThread.Start();
+}
+
+void Print()
+{
+    lock (_lockObj)  // начало критической секции
+    {
+        x = 1;
+        for (int i = 1; i < 5; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            x++;
+            Thread.Sleep(100);
+        }
+    }  // завершение критической секции
+}
 /* Пример 9. Аналог примера 8. Метод монитора Enter (object obj, bool acquiredLock) 
 int x = 0;
 object obj = new();
